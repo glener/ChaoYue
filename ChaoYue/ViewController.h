8ce4a923fc9d1b976e8cb76e57a36b88a45f7591
glener <again@mac-mini.lan>
2017-05-02 22:19:46 +0800

    Initial Commit

---
+++ ChaoYue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChaoYue
+//
+//  Created by again on 2017/5/2.
+//  Copyright © 2017年 again. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChaoYue/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ChaoYue
+//
+//  Created by again on 2017/5/2.
+//  Copyright © 2017年 again. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
